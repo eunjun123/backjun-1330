8fe7829be4d3f686a1aa7921acd58e0b99b0b45a
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:41:15 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using static System.Console;
+
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]); // Console.ReadLine()은tring 형식이기때문에변환필요
+            int B = int.Parse(input[1]);
+
+            if (A > B)
+            {
+                Console.WriteLine(">");
+            }
+
+            else if (A < B)
+            {
+                Console.WriteLine("<");
+            }
+
+            else
+            {
+                Console.WriteLine("==");
+            }
+
+        }
+
+    }
